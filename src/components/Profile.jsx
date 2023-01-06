1cbf54148fe029389f3da7e386d51e23181a790c
NiallSexton <niall.sxton@gmail.com>
2023-01-06 00:23:48 +0000

    updated background

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,6 +3,9 @@ const Profile = ({user}) => {
     <div>
         <h2>You are logged in as {user}</h2>
         </div>
+        <div>
+            <img src="https://www.canva.com/design/DAFUqF4ZE2k/N-RC7Z6E6wg395KPsMSCcw/edit?utm_content=DAFUqF4ZE2k&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" alt="" />
+        </div>
         </>
 }
 
